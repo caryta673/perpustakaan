a9e41ccb9c21d92ff51a423f3a08657447459c17
caryta673 <77711141+caryta673@users.noreply.github.com>
2021-03-15 13:43:02 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,8 @@
+
+<p></p>
+<body>
+    <center><font size="+1" face="arial">Sistem Informasi Perpustakaan</font></center>
+    <center><font size="-1" face="arial">SMK N 6 SURAKARTA</font></center>
+    <center><img src="perpusgambar.jpg" alt="perpusgambar" style="width:70%;min-height:100px"></center>
+    
+</body>
